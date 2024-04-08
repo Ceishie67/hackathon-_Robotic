e14ecb59a72739acd1b6b9177f3689c7dcc3ba25
nelio <elio.nguingnang@ynov.com>
2024-04-08 14:27:56 +0200

    servomoteur

---
+++ servomoteur.py
@@ -0,0 +1,25 @@
+from machine import Pin,PWM
+import time
+
+sg90 = PWM(Pin(14, mode=Pin.OUT))
+sg90.freq(50)
+sg91 = PWM(Pin(12, mode=Pin.OUT))
+sg91.freq(50)
+
+
+# 0.5ms/20ms = 0.025 = 2.5% duty cycle
+# 2.4ms/20ms = 0.12 = 12% duty cycle
+
+# 0.025*1024=25.6
+# 0.12*1024=122.88
+
+while True:
+    sg90.duty(20)
+    time.sleep(1)
+    sg90.duty(137)
+    time.sleep(1)
+    
+    sg91.duty(20)
+    time.sleep(1)
+    sg91.duty(123)
+    time.sleep(1)
