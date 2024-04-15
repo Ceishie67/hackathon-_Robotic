e5a65028e538f7e00c5c4c620f2280997592e3ee
nelio <elio.nguingnang@ynov.com>
2024-04-15 14:31:16 +0200

    mmmmmmmmmmm

--- Prog/actions.py
+++ Prog/actions.py
@@ -9,34 +9,36 @@ def forward(nbstep) :
 
     #jambe gauche
     while nbstep > 0:
-        rotation_Rankle_half_pos_turn(0.1) # cheville droite ==> vers la gauche pour la stabilite
+        rotation_Rankle_half_pos_turn(0.01) # cheville droite ==> vers la gauche pour la stabilite
         #
-        rotation_Lankle_half_neg_turn(0.1) # cheville gauche ==> vers la gauche / debut du 1er mouvement
+        rotation_Lankle_half_neg_turn(0.01) # cheville gauche ==> vers la gauche / debut du 1er mouvement
         #
-        rotation_Lhip_half_neg_turn(0.1) # hanche gauche ==> vers l'avant
+        rotation_Lhip_half_neg_turn(0.01) # hanche gauche ==> vers l'avant
         #
-        rotation_Rhip_half_neg_turn(0.1) # hanche droite / pied d'appuie ==> vers l'arriere / mouvement vers l'avant
+        rotation_Rhip_half_neg_turn(0.01) # hanche droite / pied d'appuie ==> vers l'arriere / mouvement vers l'avant
         #
-        rotation_Rankle_return_neutral_pos(0.1) # retoure de la cheville droite en position neutre
+        rotation_Lankle_return_neutral_neg(0.01) # retoure de la cheville gauche en position neutre
         #
-        rotation_Lankle_return_neutral_neg(0.1) # retoure de la cheville gauche en position neutre / fin 1er mouvement
+        rotation_Rankle_return_neutral_pos(0.01) # retoure de la cheville droite en position neutre
+        #
+        rotation_Rhip_return_neutral_neg(0.01) # retour de la hanche droite en neutre
+        #
+        rotation_Lhip_return_neutral_neg(0.01) # retour de la hanche gauche en neutre/ fin 1er mouvement
+        #
         rotation_Lankle_half_pos_turn(0.01) # cheville gauche ==> vers la droite pour la stabilite
         #
         rotation_Rankle_half_neg_turn(0.01) # cheville droite ==> vers la droite / debut du 2eme mouvement
         #
-        rotation_Lhip_return_neutral_neg(0.01) # retour de la hanche gauche en neutre
-        #
-        rotation_Rhip_return_neutral_neg(0.01) # retour de la hanche droite en neutre
         rotation_Rhip_half_pos_turn(0.01) # hanche droite ==> vers l'avant
         #
-        rotation_Lhip_half_neg_turn(0.01) # hanche gauche / pied d'appuie ==> vers l'arriere / mouvement vers l'avant
+        rotation_Lhip_half_pos_turn(0.01) # hanche gauche / pied d'appuie ==> vers l'arriere / mouvement vers l'avant
         #
-        rotation_Lankle_return_neutral_pos(0.01) # retoure de la cheville droite en position neutre
-        rotation_Rankle_return_neutral_neg(0.01) # retoure de la cheville gauche en position neutre / fin 2eme mouvement
+        rotation_Rankle_return_neutral_neg(0.01) # retoure de la cheville gauche en position neutre 
+        rotation_Lankle_return_neutral_pos(0.01) # retoure de la cheville droite en position neutre / fin 2eme mouvement
         #
         rotation_Rhip_return_neutral_pos(0.01) # retour de la hanche droite en neutre
         #
-        rotation_Lhip_return_neutral_neg(0.01) # retour de la hanche gauche en neutre / fin du mouvement vers l'avant
+        rotation_Lhip_return_neutral_pos(0.01) # retour de la hanche gauche en neutre / fin du mouvement vers l'avant
         #
 
 
